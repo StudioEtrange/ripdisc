a6c6e865eeb4f2d8de818da099147f460c0e574c
StudioEtrange <nomorgan@gmail.com>
2019-09-28 22:12:40 +0200

    update

--- ripdisc.sh
+++ ripdisc.sh
@@ -16,29 +16,41 @@ _CURRENT_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
 
-#DISC_DEVICE="/dev/cdrom"
 MOUNT_POINT="$_CURRENT_FILE_DIR/media"
-OUTPUT_FILENAME="$1"
+OUTPUT_FILENAME="$3"
+
 
 if [ "$1" = "-h" ]; then
 	echo "$0 -h : help."
-	echo "$0 device <device> <output_file[.m4v|mkv]> : first arg : device where disc is inserted (ie /dev/cdrom). second arg : specify a path to an output filename with an extension of m4v (DEFAULT) or mkv"
-	echo "$0 iso <iso path> <output_file[.m4v|mkv]> : first arg : iso path (ie /foo/bar/file.iso). second arg : specify a path to an output filename with an extension of m4v (DEFAULT) or mkv"
-	echo "$0 folder <dvd folder> <output_file[.m4v|mkv]> : first arg : folder where DVD files (AUDIO_TS and VIDEO_TS folders) resides (ie /foo/bar). second arg : specify a path to an output filename with an extension of m4v (DEFAULT) or mkv"
+	echo "$0 disc <device> <output_file[.m4v|mkv] path> : first arg : device where disc is inserted (ie /dev/cdrom). second arg : specify a path to an output filename with an extension of m4v (DEFAULT) or mkv"
+	echo "$0 iso <iso path> <output_file[.m4v|mkv] path> : first arg : iso path (ie /foo/bar/file.iso). second arg : specify a path to an output filename with an extension of m4v (DEFAULT) or mkv"
+	echo "$0 folder <dvd folder> <output_file[.m4v|mkv] path> : first arg : folder where DVD files (AUDIO_TS and VIDEO_TS folders) resides (ie /foo/bar). second arg : specify a path to an output filename with an extension of m4v (DEFAULT) or mkv"
 	exit
 fi
 
 if [ "$1" = "" ]; then
 	echo "$0 -h for help."
+	echo "NOTE : run as sudo"
+	echo "SAMPLES :"
+	echo '	sudo ./ripdisc.sh folder "../download/complete/movie/MOVIE_SPECIAL_EDITION" ../output/movie_multi_dvdrip.mkv'
+	echo '	sudo ./ripdisc.sh disc "/dev/cdrom" ../output/movie_multi_dvdrip.mkv'
+	echo '	sudo ./ripdisc.sh iso "../download/iso/movie.iso" ../output/movie_multi_dvdrip.mkv'
+
 	exit 1
 fi
 
 
 echo "Hello.."
 
+# NOTE: this is  here because encode.pl script have hardcoded localtion of HandBrakeCli..
+mkdir -p $HOME/bin/video
+ln -s $(which HandBrakeCLI) $HOME/bin/video/HandBrakeCLI
+
+
+
 mkdir -p "$MOUNT_POINT"
 
-if [ "$1" = "device" ]; then
+if [ "$1" = "disc" ]; then
 	# Closing disc
 	eject -t 2>/dev/null
 	sleep 15
@@ -72,7 +84,7 @@ date1=$(date +"%s")
 #AUDIO_EXCLUDE_REGEX="ACCEPT_ALL"
 #AUDIO_EXCLUDE_REGEX='\b(?:Czech|Deutsch|Italiano|Magyar|Polish|Portugues|Thai|Turkish)\b'
 # SUB_INCLUDE_REGEX='\b(?:English|Francais|Unknown|Closed\s+Captions)\b'
-DEBUG="1" AUDIO_COPY="1" SUB_INCLUDE_REGEX='\b(?:.*)\b' AUDIO_EXCLUDE_REGEX='ACCEPT_ALL' \
+HB_EXEC="$(which HandBrakeCLI)" DEBUG="1" AUDIO_COPY="1" SUB_INCLUDE_REGEX='\b(?:.*)\b' AUDIO_EXCLUDE_REGEX='ACCEPT_ALL' \
 $_CURRENT_FILE_DIR/encode.pl "$MOUNT_POINT" "$OUTPUT_FILENAME" main
 
 date2=$(date +"%s")
@@ -81,12 +93,12 @@ echo "Process ended."
 diff=$(($date2-$date1))
 echo "Time elpased : $(($diff / 60)) minutes and $(($diff % 60)) seconds."
 
-ls -al "$OUTPUT_FILENAME"*
+ls -al "$OUTPUT_FILENAME"
 
 
 
 
-if [ "$1" = "device" ]; then
+if [ "$1" = "disc" ]; then
 	echo "Unmounting Disc."
 	umount "$DISC_DEVICE"
 	eject
